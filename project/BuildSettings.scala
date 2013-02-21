5d7a4b8e3c05844502330fc755593244fd0bc8e3
Johannes Rudolph <johannes_rudolph@gmx.de>
2013-02-21 14:22:02 +0100

    build: set a new spray version that will override 1.1-M7.1 by usual ivy rules

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,7 +4,7 @@ import ls.Plugin._
 
 
 object BuildSettings {
-  val VERSION = "1.1-M7-openssl-M2"
+  val VERSION = "1.1-M7.1-openssl-M2"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
