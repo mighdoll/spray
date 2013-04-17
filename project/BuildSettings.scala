022b33eed03929118ae7e1872e5da1be4058f0a4
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-04-17 15:57:49 +0200

    bump version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,7 +4,7 @@ import ls.Plugin._
 
 
 object BuildSettings {
-  val VERSION = "1.1-M7.1-openssl-M4-timing"
+  val VERSION = "1.1-M7.1-openssl-M4.1"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
